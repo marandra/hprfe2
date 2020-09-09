b9eabb8db8c174b655a6baf89b3f8df7f6f0d9d7
marandra <marcelo.raschi@gmail.com>
2020-09-09 14:35:27 +0200

    Add dummy test as workaround for pytest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hprfe2", # Replace with your own username
+    name="hprfe2", # Replace with your username in test server
     version="0.0.1",
     author="Marcelo RAschi",
     author_email="author@example.com",
