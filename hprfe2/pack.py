2e359d3784f058e2d4abc3432a04ed7e53d3a958
marandra <marcelo.raschi@gmail.com>
2020-09-14 19:49:26 +0200

    Add pack and roc modules

---
+++ hprfe2/pack.py
@@ -0,0 +1,218 @@
+"""
+PACK: pending description here.
+"""
+
+import json
+from pathlib import Path
+import numpy
+#import KratosMultiphysics
+#from KratosMultiphysics.StructuralMechanicsApplication import (
+#    structural_mechanics_analysis,
+#)
+import logging
+from common import Common
+
+logging.basicConfig(
+    format="[%(asctime)s] %(message)s", datefmt="%H:%M:%S", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+
+
+def write_json(filename, data_dict):
+    with open(filename, "w") as fo:
+        json.dump(data_dict, fo, indent=2)
+
+
+def read_json(filename):
+    with open(filename) as f:
+        data_dict = json.load(f)
+    return data_dict
+
+
+def get_properties(rve_modelpart, iw_list):
+    # read model materials
+    out_prop = []
+    for l in iw_list:
+        elem_id = int(l[0])
+        elem = rve_modelpart.GetElement(elem_id)
+        prop_id = elem.Properties.Id
+        out_prop.append(prop_id)
+    return out_prop
+
+
+def unpack_ip_data(iw_list):
+    out_e = []
+    out_ip = []
+    out_w = []
+    out_gip = []
+    for l in iw_list:
+        out_e.append(int(l[0]))
+        out_ip.append(int(l[1]))
+        out_w.append(float(l[2]))
+        out_gip.append(int(l[3]))
+    return out_e, out_ip, out_w, out_gip
+
+
+def parse_strain_bases(strain_bases_filename, iw_list, nr_modes):
+    strain_bases = numpy.load(str(strain_bases_filename), mmap_mode="r")
+    strain_bases = strain_bases[:, :nr_modes]
+    nr_comps = 6
+    out_B = []
+    for l in iw_list:
+        gip = int(l[3])
+        index = gip * nr_comps
+        B = strain_bases[index : index + nr_comps, :]
+        out_B.append(B.tolist())
+    return out_B
+
+
+def create_rve_params_structure(
+    strain_bases_filename,
+    rve_materials_filename,
+    nr_modes,
+    reduced_ip_set,
+    rve_modelpart,
+):
+    """ gather and pack IP data for RVE constitutive law """
+    rve_params = {}
+    out_e, out_lip, out_w, out_gip = unpack_ip_data(reduced_ip_set)
+    # required data
+    rve_params["ip_global_id"] = out_gip
+    rve_params["ip_weight"] = out_w
+    rve_params["ip_property_id"] = get_properties(rve_modelpart, reduced_ip_set)
+    rve_params["ip_strain_modes"] = parse_strain_bases(
+        strain_bases_filename, reduced_ip_set, nr_modes
+    )
+    rve_params["material_parameters"] = read_json(rve_materials_filename)
+    #  metadata
+    rve_params["nr_modes"] = nr_modes
+    rve_params["nr_reduced_ip"] = len(reduced_ip_set)
+    rve_params["ip_element_id"] = out_e  # TODO: check if we use this data
+    rve_params["ip_local_id"] = out_lip  # TODO: check if we use this data
+    return rve_params
+
+
+def write_datasets(common):
+    """ docstring here """
+
+    import KratosMultiphysics
+    from KratosMultiphysics.StructuralMechanicsApplication import (
+        structural_mechanics_analysis,
+    )
+
+    parameters_dict = {
+        "problem_data": {
+            "problem_name": "High_Fidelity",
+            "parallel_type": "OpenMP",
+            "start_time": 0.0,
+            "end_time": 0.99,
+            "echo_level": 1,
+        },
+        "solver_settings": {
+            "model_part_name": "Microstructure",
+            "domain_size": 3,
+            "echo_level": 1,
+            "time_stepping": {},
+            "solver_type": "Static",
+            "model_import_settings": {
+                "input_type": "mdpa",
+                "input_filename": "{}/model".format(common.training_path),
+            },
+            "material_import_settings": {
+                "materials_filename": "{}/materials.json".format(common.training_path)
+            },
+        },
+    }
+
+    parameters = KratosMultiphysics.Parameters(json.dumps(parameters_dict))
+    model = KratosMultiphysics.Model()
+    simulation = structural_mechanics_analysis.StructuralMechanicsAnalysis(
+        model, parameters
+    )
+    simulation.Initialize()
+    modelpart = simulation._GetSolver().GetComputingModelPart()
+
+    materials_fname = common.materials_fname
+    for p in common.ip_subsets:
+        roc_filename = common.bases_path / common.roc_fname(p)
+        ip_set = numpy.loadtxt(roc_filename)
+        for m in common.config["rve_data_modes"]:
+            rve_fname = common.datasets_path / common.rve_fname(m, p)
+            if common.skip_calculation(rve_fname):
+                logger.info("File {} exists. Skipping calculation".format(rve_fname))
+                continue
+            logger.info("Generating {}".format(rve_fname))
+            rve_params = create_rve_params_structure(
+                common.get_bases_fname(common.config["strain_name"]),
+                materials_fname,
+                m,
+                ip_set,
+                modelpart,
+            )
+            write_json(rve_fname, rve_params)
+
+    return
+
+#######################################################################
+#######################################################################
+
+if __name__ == "__main__":
+
+    import sys
+
+    if len(sys.argv) > 1:
+        C = Common(root_path=Path(sys.argv[1]))
+    else:
+        exit("Missing root_path argument.")
+
+    write_datasets(C)
+#    parameters_dict = {
+#        "problem_data": {
+#            "problem_name": "High_Fidelity",
+#            "parallel_type": "OpenMP",
+#            "start_time": 0.0,
+#            "end_time": 0.99,
+#            "echo_level": 1,
+#        },
+#        "solver_settings": {
+#            "model_part_name": "Microstructure",
+#            "domain_size": 3,
+#            "echo_level": 1,
+#            "time_stepping": {},
+#            "solver_type": "Static",
+#            "model_import_settings": {
+#                "input_type": "mdpa",
+#                "input_filename": "{}/model".format(co.training_path),
+#            },
+#            "material_import_settings": {
+#                "materials_filename": "{}/materials.json".format(co.training_path)
+#            },
+#        },
+#    }
+#
+#    parameters = KratosMultiphysics.Parameters(json.dumps(parameters_dict))
+#    model = KratosMultiphysics.Model()
+#    simulation = structural_mechanics_analysis.StructuralMechanicsAnalysis(
+#        model, parameters
+#    )
+#    simulation.Initialize()
+#    modelpart = simulation._GetSolver().GetComputingModelPart()
+#
+#    materials_fname = co.materials_fname
+#    for p in co.ip_subsets:
+#        roc_filename = co.roc_fname(p)
+#        ip_set = numpy.loadtxt(roc_filename)
+#        for m in co.context["rve_data_modes"]:
+#            rve_fname = co.rve_fname(m, p)
+#            if co.skip_calculation(rve_fname):
+#                logger.info("File {} exists. Skipping calculation".format(rve_fname))
+#                continue
+#            logger.info("Generating {}".format(rve_fname))
+#            rve_params = create_rve_params_structure(
+#                co.get_bases_fname(co.context["strain_name"]),
+#                materials_fname,
+#                m,
+#                ip_set,
+#                modelpart,
+#            )
+#            write_json(rve_fname, rve_params)
---
+++ hprfe2/roc.py
@@ -0,0 +1,277 @@
+"""
+ROC: pending description here.
+"""
+from pathlib import Path
+import json
+import logging
+import numpy as np
+
+#import KratosMultiphysics
+#from KratosMultiphysics.StructuralMechanicsApplication import (
+#    structural_mechanics_analysis,
+#)
+
+from common import Common
+
+
+logging.basicConfig(
+    format="[%(asctime)s] %(message)s", datefmt="%H:%M:%S", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+
+
+###########################################################
+#   Reduced Order Cubeture algorithm
+###########################################################
+
+
+def remove_exact_integral_energy(modes, weights):
+    eps = np.finfo(float).eps  # 2.22044604925e-16
+    # Total microscale volume
+    total_weight = np.sum(weights)
+    sqrt_total_weight = np.sqrt(total_weight)
+    sqrt_weights = np.sqrt(weights)
+    # Normalized exact integral
+    norm_exact_integral = modes.T @ weights / total_weight
+    # Matrix of modified modes (with zero integral)
+    modified_modes = (modes - norm_exact_integral) * sqrt_weights.reshape(-1, 1)
+    [modified_modes, bases_weights] = np.linalg.svd(
+        modified_modes, full_matrices=False
+    )[:2]
+    # filter the reduced modified set of modes
+    tolerance = np.max(modes.shape) * eps * np.max(bases_weights)
+    rank_mod_modes = sum(i > tolerance for i in bases_weights)
+    modified_modes = modified_modes[:, 0:rank_mod_modes]
+    # Adding last row related with the sqrt of gauss integration weigths
+    # and initializing the RHS vector for the optimization problem
+    J = np.vstack([modified_modes.T, (sqrt_weights / sqrt_total_weight).T])
+    b = np.vstack([np.zeros((modified_modes.T.shape[0], 1)), sqrt_total_weight])
+    return J, b
+
+
+def update_weights_inverse(H, alpha, bases_current, base_new, r):
+    c = np.dot(bases_current.T, base_new)
+    d = np.dot(H, c).reshape(-1, 1)
+    s = np.dot(base_new.T, base_new) - np.dot(c.T, d)
+    aux1 = np.hstack([H + np.outer(d, d) / s, -d / s])
+    aux2 = np.hstack([np.squeeze(-d.T / s), 1 / s])
+    H_new = np.vstack([aux1, aux2])
+    v = np.dot(base_new.T, r) / s
+    alpha = np.vstack([(alpha - d * v), v])
+    return H_new, alpha
+
+
+def update_inverse_hermitian(invH, neg_index):
+    if neg_index == np.shape(invH)[1]:
+        aux = (invH[0:-1, -1] * invH[-1, 0:-1]) / invH(-1, -1)
+        invH_new = invH[:-1, :-1] - aux
+    else:
+        aux1 = np.hstack(
+            [
+                invH[:, 0:neg_index],
+                invH[:, neg_index + 1 :],
+                invH[:, neg_index].reshape(-1, 1),
+            ]
+        )
+        aux2 = np.vstack(
+            [aux1[0:neg_index, :], aux1[neg_index + 1 :, :], aux1[neg_index, :]]
+        )
+        invH_new = (
+            aux2[0:-1, 0:-1] - np.outer(aux2[0:-1, -1], aux2[-1, 0:-1]) / aux2[-1, -1]
+        )
+    return invH_new
+
+
+def multiupdate_inverse_hermitian(invH, neg_indexes):
+    neg_indexes = np.sort(neg_indexes)
+    for i in range(np.size(neg_indexes)):
+        neg_index = neg_indexes[i] - i
+        invH = update_inverse_hermitian(invH, neg_index)
+    return invH
+
+
+def compute_roq(Modes, weights, nGP, tol):
+    J, b = remove_exact_integral_energy(Modes, weights)
+    y = np.arange(len(weights))
+    r = b  # residual vector, initial guess
+    it = 0  # number of iterations
+    mPOS = 0  # number of non-zero weights
+    z = []
+    Jnorm = np.sqrt(sum(np.multiply(J, J), 0))
+
+    # point selection algorithm
+    while (np.linalg.norm(r) / np.linalg.norm(b) > tol) and (mPOS <= nGP):
+        # 1. Compute new point
+        ObjFun = np.dot((J[:, y]).T, r)
+        div = np.multiply(Jnorm[y], np.linalg.norm(r)).reshape(-1, 1)
+        ObjFun = np.divide(ObjFun, div)
+        s = ObjFun.argmax()
+        i = y[s]
+        # 2. Update alpha and H (unrestricted least squares)
+        if it == 0:
+            # complies with newer versions of numpy
+            alpha = np.linalg.lstsq(J[:, [i]], b, rcond=None)[0]
+            # alpha = np.linalg.lstsq(J[:, [i]], b)[0]
+            H = 1 / np.dot((J[:, i]).T, J[:, i])
+        else:
+            H, alpha = update_weights_inverse(H, alpha, J[:, z], J[:, i], r)
+        # 3. Move i from set y to set z
+        z = (np.append(z, i)).astype(int)
+        y = np.delete(y, s)
+        # 4. Find possible negative weights
+        if any(alpha < 0):
+            logger.warning("NEGATIVE weight found")
+            indexes_neg_weight = np.where(alpha <= 0.0)[0]
+            y = np.append(y, (z[indexes_neg_weight]).T)
+            z = np.delete(z, indexes_neg_weight)
+            H = multiupdate_inverse_hermitian(H, indexes_neg_weight)
+            alpha = np.dot(H, np.dot(J[:, z].T, b))
+
+        # 6. Update the residual
+        r = b - np.dot(J[:, z], alpha)
+        # 7. Update mPOS and k
+        mPOS = np.size(z)
+        it = it + 1
+        logger.debug(
+            "k = {}, mPOS = {}, error = {:.2f}%".format(
+                it, mPOS, np.linalg.norm(r) / np.linalg.norm(b) * 100
+            )
+        )
+    # 6. Postprocess of points - neglecting null weights
+    w = np.multiply(alpha, np.sqrt(weights[z]).reshape(-1, 1))
+    logger.debug("Reduced Weights: {}".format(w.T))
+    logger.debug("sum of reduced weights: {}".format(np.sum(w)))
+    logger.debug("IP's index (ids starts from zero): {}".format(z))
+    return w, z
+
+
+###########################################################
+#   End of algorithm
+###########################################################
+
+
+def compute_hprom_weights(ip_data, nr_roq_points, energy_bases_filename):
+    logger.info("Computing reduced set of integration points (HPROM)")
+    ip_weights = ip_data[0]
+    ip_lids = ip_data[1]
+    elem_ids = ip_data[2]
+    energy_modes = np.load(energy_bases_filename)[:, :nr_roq_points]
+    [w, z] = compute_roq(energy_modes, np.array(ip_weights), nr_roq_points, tol=1.0e-14)
+    roq_list = []
+    for x, ip_gid in enumerate(z):
+        ip_lid = ip_lids[ip_gid]
+        elem_id = elem_ids[ip_gid]
+        roq_list.append([elem_id, ip_lid, w[x][0], ip_gid])
+    return roq_list  # returns list of: element id, local IP id, IP weight, global IP id
+
+
+def compute_rom_weights(ip_data):
+    logger.info("Computing complete set of integration points (ROM)")
+    ip_weights = ip_data[0]
+    ip_lids = ip_data[1]
+    elem_ids = ip_data[2]
+    roq_list = []
+    for ip_gid in range(len(ip_weights)):
+        ip_weight = ip_weights[ip_gid]
+        ip_lid = ip_lids[ip_gid]
+        elem_id = elem_ids[ip_gid]
+        roq_list.append([elem_id, ip_lid, ip_weight, ip_gid])
+    return roq_list
+
+
+def write_ip_sets(common):
+    """ docstring here """
+
+    import KratosMultiphysics
+    from KratosMultiphysics.StructuralMechanicsApplication import (
+        structural_mechanics_analysis,
+    )
+
+    parameters_dict = {
+        "problem_data": {
+            "problem_name": "High_Fidelity",
+            "parallel_type": "OpenMP",
+            "start_time": 0.0,
+            "end_time": 0.99,
+            "echo_level": 1,
+        },
+        "solver_settings": {
+            "model_part_name": "Microstructure",
+            "domain_size": 3,
+            "echo_level": 1,
+            "time_stepping": {},
+            "solver_type": "Static",
+            "model_import_settings": {
+                "input_type": "mdpa",
+                "input_filename": "{}/model".format(common.training_path),
+            },
+            "material_import_settings": {
+                "materials_filename": "{}/materials.json".format(common.training_path)
+            },
+        },
+    }
+
+    parameters = KratosMultiphysics.Parameters(json.dumps(parameters_dict))
+    model = KratosMultiphysics.Model()
+    simulation = structural_mechanics_analysis.StructuralMechanicsAnalysis(
+        model, parameters
+    )
+    simulation.Initialize()
+    rve_modelpart = simulation._GetSolver().GetComputingModelPart()
+
+    #
+    # gather global model part info
+    #
+    ip_weights = []
+    ip_lids = []
+    elem_ids = []
+    for elem in rve_modelpart.Elements:
+        iw_list = elem.CalculateOnIntegrationPoints(
+            KratosMultiphysics.INTEGRATION_WEIGHT, rve_modelpart.ProcessInfo
+        )
+        for ip_lid, ip_weight in enumerate(iw_list):
+            ip_weights.append(ip_weight)
+            ip_lids.append(ip_lid)
+            elem_ids.append(elem.Id)
+    ip_data = [ip_weights, ip_lids, elem_ids]
+    nr_ips = len(ip_data[0])
+
+    #
+    # compute ip set
+    #
+    for nr_p in common.ip_subsets:
+        roc_filename = common.bases_path / common.roc_fname(nr_p)
+        if common.skip_calculation(roc_filename):
+            logger.info("File {} exists. Skipping calculation".format(roc_filename))
+            continue
+        if "ROM" in str(nr_p):  # ROM case
+            roc_list = compute_rom_weights(ip_data)
+        else:  # HPROM case
+            logger.info("Generating {}".format(roc_filename))
+            # compute ROC list
+            energy_bases_fname = common.get_bases_fname(common.config["energy_name"])
+            if energy_bases_fname is None:
+                logger.error("Missing energy bases file. Exiting.")
+                exit()
+            roc_list = compute_hprom_weights(ip_data, nr_p, energy_bases_fname)
+
+        with open(roc_filename, "w") as ofile:
+            for list in roc_list:
+                ofile.write("{} {} {} {}\n".format(list[0], list[1], list[2], list[3]))
+
+    return
+
+#######################################################################
+# Main
+#######################################################################
+
+if __name__ == "__main__":
+
+    import sys
+
+    if len(sys.argv) > 1:
+        C = Common(root_path=Path(sys.argv[1]))
+    else:
+        exit("Missing root_path argument.")
+
+    write_ip_sets(C)
